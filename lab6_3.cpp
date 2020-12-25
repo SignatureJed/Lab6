bff5c921f7c1121968a4d3dae1a4457cfaa7bad7
SignatureJed <kong.3392.th@gmail.com>
2020-12-25 16:37:44 +0700

    lab6_3.cpp

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -1,8 +1,24 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+char before (char x)
+{
+     if (x<65||x>=91)
+     {
+        return '0';
+     }
+     if (x==65)
+     {
+        return 90;
+     }
+     else
+     {
+         (x=x-1);
+         
+     }
+     
+     return x;
+
 }
 
 int main(){
